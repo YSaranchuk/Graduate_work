ca13505f8c81dc3536312c0f74ee5a539d8d042f
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-07-03 23:35:36 +0300

    Update config.php

--- config.php
+++ config.php
@@ -8,18 +8,3 @@ return [
 		'pass' => '',
 	]
 ];
-//вынесу в отдельный файл
-<?php
-
-class DataBase {
-	public static function connect ($host, $dbname, $user, $pass) {
-		try {
-			$db = new PDO('mysql:host='.$host.'; dbname='.$dbname.'; charset=utf8', $user, $pass);
-		}
-		catch (PDOException $e) {
-			die('Database error: '.$e->getMessage().'<br>');
-		}
-		
-		return $db;
-	}
-}
