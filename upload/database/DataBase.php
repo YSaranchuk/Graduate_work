3c7ab60973bf3af0f6ee36de0d28a8d6ae11168a
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-07-14 12:36:22 +0300

    Update DataBase.php

--- upload/database/DataBase.php
+++ upload/database/DataBase.php
@@ -3,7 +3,7 @@
 class DataBase {
 	public static function connect ($host, $dbname, $user, $pass) {
 		try {
-			$db = new PDO('mysql:host='.$host.'; dbname='.$dbname.'; charset=utf8', $user, $pass);
+			$db = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $pass);
 		}
 		catch (PDOException $e) {
 			die('Database error: '.$e->getMessage().'<br>');
