b6fa23ddbe54e236f5c2515fbe563d739ebb0833
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-07-15 14:03:29 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
 	'mysql' => [
-		'host' => 'university.netology.ru',
+		'host' => 'http://university.netology.ru/u/saranchuk/',
 		'dbname' => 'faq',
 		'user' => 'saranchuk',
 		'pass' => 'neto1769',
