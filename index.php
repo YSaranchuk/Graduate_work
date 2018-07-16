80a5198cd1a4a404e068022852ddc73fece339ef
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-07-17 00:36:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php
+
+require_once __DIR__ . '/vendor/autoload.php';
 session_start();
 ini_set('error_reporting', E_ALL & ~E_NOTICE);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-
-
 $config = include __DIR__ . '/config.php';
-
 include 'upload/database/DataBase.php';
-
 $db = DataBase::connect(
 	$config['mysql']['host'],
 	$config['mysql']['dbname'],
 	$config['mysql']['user'],
 	$config['mysql']['pass']
 );
-
 include 'upload/router/router.php';
