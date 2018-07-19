2e2c891ff7401392dea86a598395879224074d70
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-07-19 14:41:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-
 require_once __DIR__ . '/vendor/autoload.php';
+
 session_start();
 ini_set('error_reporting', E_ALL & ~E_NOTICE);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 $config = include __DIR__ . '/config.php';
-include 'upload/database/DataBase.php';
-$db = DataBase::connect(
+include '/upload/database/DataBase.php';
+$db = \Graduate\Database\DataBase::connect(
 	$config['mysql']['host'],
 	$config['mysql']['dbname'],
 	$config['mysql']['user'],
