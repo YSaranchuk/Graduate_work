80aecbc7e59e5a4dc826d70bc45147c49b6efd1a
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-07-19 14:53:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ ini_set('error_reporting', E_ALL & ~E_NOTICE);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 $config = include __DIR__ . '/config.php';
-include '/upload/database/DataBase.php';
+include 'upload/database/DataBase.php';
 $db = \Graduate\Database\DataBase::connect(
 	$config['mysql']['host'],
 	$config['mysql']['dbname'],
