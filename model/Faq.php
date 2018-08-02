cfba7672919127831a33dee495e707b6f2327cd8
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-08-02 18:01:30 +0300

    Update Faq.php

--- model/Faq.php
+++ model/Faq.php
@@ -11,14 +11,14 @@ class Faq {
 	public function getListAdmin() {
 		$sql = "SELECT login, password, id FROM users";
 		$allUser = $this->db->query($sql);
-		$allUser->setFetchMode(PDO::FETCH_ASSOC);
+		$allUser->setFetchMode(\PDO::FETCH_ASSOC);
 		return $allUser->fetchAll();
 	}
 	
 	public function deleteQuestion($delId) {
 		$sql = "DELETE FROM question WHERE id = :id";
 		$del = $this->db->prepare($sql);
-		$del->bindValue(':id', $delId, PDO::PARAM_INT);
+		$del->bindValue(':id', $delId, \PDO::PARAM_INT);
 		$del->execute();
 		header('Location: ?interface-admin=1&showQuestion='.$_GET['showQuestion']);
 	}
@@ -26,11 +26,11 @@ class Faq {
 	public function deleteCategoryAndQuestion($id) {
 		$delCategory = "DELETE FROM category WHERE id = :id";
 		$delCat = $this->db->prepare($delCategory);
-		$delCat->bindValue(':id', $id, PDO::PARAM_INT);
+		$delCat->bindValue(':id', $id, \PDO::PARAM_INT);
 		
 		$delQuestion = "DELETE FROM question WHERE list_id = :id";
 		$delQuest = $this->db->prepare($delQuestion);
-		$delQuest->bindValue(':id', $id, PDO::PARAM_INT);
+		$delQuest->bindValue(':id', $id, \PDO::PARAM_INT);
 		
 		$delCat->execute();
 		$delQuest->execute();
@@ -48,7 +48,7 @@ class Faq {
 			FROM question INNER JOIN category WHERE category.id = '".$id."'  AND question.list_id = '".$id."' GROUP BY question.name";
 			
 		$getQuestion = $this->db->query($sql);
-		$getQuestion->setFetchMode(PDO::FETCH_ASSOC);
+		$getQuestion->setFetchMode(\PDO::FETCH_ASSOC);
 		return $getQuestion->fetchAll();	
 	}
 	
@@ -64,49 +64,49 @@ class Faq {
 				WHERE question.answer = '' AND category.id = question.list_id GROUP BY question.name ORDER BY question.data";
 			
 		$getQuestionNoAnswer = $this->db->query($sql);
-		$getQuestionNoAnswer->setFetchMode(PDO::FETCH_ASSOC);
+		$getQuestionNoAnswer->setFetchMode(\PDO::FETCH_ASSOC);
 		return $getQuestionNoAnswer->fetchAll();	
 	}
 	
 	public function hiddenQuestion($id) {
 		$sql = "UPDATE question SET `status` = 2 WHERE id = :id";
 		$hidd = $this->db->prepare($sql);
-		$hidd->bindValue(':id', $id, PDO::PARAM_INT);
+		$hidd->bindValue(':id', $id, \PDO::PARAM_INT);
 		$hidd->execute();
 	}
 	
 	public function questionShow($id) {
 		$sql = "UPDATE question SET `status` = 1 WHERE id = :id";
 		$show = $this->db->prepare($sql);
-		$show->bindValue(':id', $id, PDO::PARAM_INT);
+		$show->bindValue(':id', $id, \PDO::PARAM_INT);
 		$show->execute();
 	}
 	
 	public function getCategory() {
 		$allCategory = 'SELECT id, name FROM category';
 		$result = $this->db->query($allCategory);
-		$result->setFetchMode(PDO::FETCH_ASSOC);
+		$result->setFetchMode(\PDO::FETCH_ASSOC);
 		return $result->fetchAll();
 	}
 	
 	public function getCategoryAndCountQuestion() {
 		$allCategory = 'SELECT category.name, category.id, count(question.name) AS count_question FROM category LEFT JOIN question ON category.id = question.list_id GROUP BY category.name ORDER BY category.id';
 		$result2 = $this->db->query($allCategory);
-		$result2->setFetchMode(PDO::FETCH_ASSOC);
+		$result2->setFetchMode(\PDO::FETCH_ASSOC);
 		return $result2->fetchAll();
 	}
 	
 	public function getCategoryAndCountShowQuestion() {
 		$allCategory = 'SELECT category.name, count(question.status) AS count_show FROM category LEFT JOIN question ON category.id = question.list_id  AND question.status = 1 GROUP BY category.name ORDER BY category.id';
 		$result2 = $this->db->query($allCategory);
-		$result2->setFetchMode(PDO::FETCH_ASSOC);
+		$result2->setFetchMode(\PDO::FETCH_ASSOC);
 		return $result2->fetchAll();
 	}
 	
 	public function getCategoryAndCountAnswerQuestion() {
 		$allCategory = 'SELECT category.name, count(question.answer) AS count_answer FROM category LEFT JOIN question ON category.id = question.list_id  AND question.answer = "" GROUP BY category.name ORDER BY category.id';
 		$result2 = $this->db->query($allCategory);
-		$result2->setFetchMode(PDO::FETCH_ASSOC);
+		$result2->setFetchMode(\PDO::FETCH_ASSOC);
 		return $result2->fetchAll();
 	}
 	
@@ -120,7 +120,7 @@ class Faq {
 		}
 		
 		$resultFaq = $this->db->query($allFaq);
-		$resultFaq->setFetchMode(PDO::FETCH_ASSOC);
+		$resultFaq->setFetchMode(\PDO::FETCH_ASSOC);
 		return $resultFaq->fetchAll();
 	}
 	
@@ -144,11 +144,11 @@ class Faq {
 																			)";
 		
 		$sth = $this->db->prepare($sql);
-		$sth->bindValue(':name', $text, PDO::PARAM_STR);
-		$sth->bindValue(':list', $list, PDO::PARAM_INT);
-		$sth->bindValue(':userName', $name, PDO::PARAM_STR);
-		$sth->bindValue(':date', $date, PDO::PARAM_INT);
-		$sth->bindValue(':email', $input_email, PDO::PARAM_STR);
+		$sth->bindValue(':name', $text, \PDO::PARAM_STR);
+		$sth->bindValue(':list', $list, \PDO::PARAM_INT);
+		$sth->bindValue(':userName', $name, \PDO::PARAM_STR);
+		$sth->bindValue(':date', $date, \PDO::PARAM_INT);
+		$sth->bindValue(':email', $input_email, \PDO::PARAM_STR);
 		$sth->execute();
 		die ('<p>Вопрос появится, после того, как на него ответит администратор</p>');															
 	}
@@ -156,8 +156,8 @@ class Faq {
 	public function editQuestion($questionId, $question) {
 		$sql = "UPDATE question SET `name` = :setQuestion WHERE id = :idQuest";
 		$setQuestion = $this->db->prepare($sql);
-		$setQuestion->bindValue(':idQuest', $questionId, PDO::PARAM_INT);
-		$setQuestion->bindValue(':setQuestion', $question, PDO::PARAM_STR);
+		$setQuestion->bindValue(':idQuest', $questionId, \PDO::PARAM_INT);
+		$setQuestion->bindValue(':setQuestion', $question, \PDO::PARAM_STR);
 		$setQuestion->execute();
 		if($_GET['showQuestion']) {
 			header('Location: ?interface-admin=1&showQuestion='. $_GET['showQuestion']);
@@ -172,12 +172,12 @@ class Faq {
 		$sql2 = "UPDATE question SET `status` = '1' WHERE id = :idQuest";
 			
 		$setAnswer = $this->db->prepare($sql);
-		$setAnswer->bindValue(':idQuest', $questionId, PDO::PARAM_INT);
-		$setAnswer->bindValue(':setAnswer', $answer, PDO::PARAM_INT);
+		$setAnswer->bindValue(':idQuest', $questionId, \PDO::PARAM_INT);
+		$setAnswer->bindValue(':setAnswer', $answer, \PDO::PARAM_INT);
 		$setAnswer->execute();
 			
 		$setStatus = $this->db->prepare($sql2);
-		$setStatus->bindValue(':idQuest', $questionId, PDO::PARAM_INT);
+		$setStatus->bindValue(':idQuest', $questionId, \PDO::PARAM_INT);
 		$setStatus->execute();
 			
 		if($_GET['showQuestion']) {
@@ -192,7 +192,7 @@ class Faq {
 		$sql = "UPDATE question SET `list_id` = :category WHERE id = :id";
 		$setCategory = $this->db->prepare($sql);
 		$setCategory->bindValue(':category', $category, PDO::PARAM_INT);
-		$setCategory->bindValue(':id', $id, PDO::PARAM_INT);
+		$setCategory->bindValue(':id', $id, \PDO::PARAM_INT);
 		$setCategory->execute();
 		header('Location: ?interface-admin=1&showQuestion='. $_GET['showQuestion']);
 	}
